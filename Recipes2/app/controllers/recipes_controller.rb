8b2b4d331da321fc248619f9550f2f011e2822ae
Ali Jordahl <ajordahl123@gmail.com>
2019-03-31 15:47:43 -0400

    Add Recipes controller and root route

---
+++ Recipes2/app/controllers/recipes_controller.rb
@@ -0,0 +1,29 @@
+class RecipesController < ApplicationController
+    def index
+        # to access all recipes in the view
+        @recipes = Recipe.all
+    end
+
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+
+
+    # NOTE: see HW4 code for guidance with the following:
+
+    # def new
+    #     @blank_recipe = Recipe.new
+    # end
+
+    # def create
+        
+    # end
+
+    # def update
+
+    # end
+
+    # def destroy
+
+    # end
+end
