a27660d3e1ca7699fef717aa588cae70a41b8201
Bec Mitchell <bmitchell@colgate.edu>
2019-04-04 20:06:05 -0400

    New review view

--- Recipes2/db/migrate/20190330192557_create_reviews.rb
+++ Recipes2/db/migrate/20190330192557_create_reviews.rb
@@ -1,12 +1,13 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
+      t.belongs_to :recipe
       t.integer :stars
       t.text :text
       t.references :recipe, foreign_key: true
       t.references :user, foreign_key: true
       #t.belongs_to :user
-      
+
       t.timestamps
     end
   end
