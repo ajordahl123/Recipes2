b75ae209d448ac4d1538511f31298681f5d2ece8
cn0017 <cnie@colgate.edu>
2019-05-01 18:30:09 -0400

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,30 +24,22 @@ end
 
 
 #recipes
-recipe_list = [['sushi','Dinner','no','no','yes','yes','Japanese','','Put things on rice','rice, fish, soy sauce, wasabi','10','Easy',2],
-	    ['spaghetti','Dinner','yes','yes','yes','yes','Italian','pot','Boil spaghetti','spaghetti','20','Medium',3],
-        ['burger','Lunch','no','no','yes','no','American','pan','This is a test','buns,grounded beef,cheese,tomato,lettuce','30','Hard',1],
-        ['chicken pot pie','Dinner','no','no','yes','no','American','pan','This is a test','chicken,pie crust,peas,carrots','30','Hard',1]
+recipe_list = [['Sushi','Dinner','false','false','true','true','Japanese','No','Put things on rice','rice, fish, soy sauce, wasabi','10','Easy',2],
+	    ['Spaghetti','Dinner','true','true','true','true','Italian','pot','Boil spaghetti','spaghetti','20','Medium',3],
+        ['Burger','Lunch','false','false','true','false','American','pan','This is a test','buns,grounded beef,cheese,tomato,lettuce','30','Hard',1],
+        ['Chicken pot pie','Dinner','false','false','true','false','American','pan','This is a test','chicken,pie crust,peas,carrots','30','Hard',1]
 	    #,'app/assets/images/sushi.jpg','app/assets/images/spaghetti.jpg'
 ]
 
 
 recipe_list.each do |name, type, is_vegan, is_vege, is_nutf, is_dairyf, cuisine,
-    appliance, instr, ingre, time, level, user_id, img_path|#img_path
+    appliance, instr, ingre, time, level, user_id|#img_path
     #img = File.read(img_path)
     Recipe.create( recipe_name: name, meal_type: type, vegan:is_vegan, vegetarian: is_vege, nut_free: is_nutf,
         dairy_free: is_dairyf, cuisine: cuisine, appliance: appliance, instructions: instr, ingredients: ingre,
-        time_to_create: time, level: level, user_id: user_id, image: File.new(img_path))#, image: Rails.root.join(img_path).open
+        time_to_create: time, level: level, user_id: user_id)#, image: File.new(img_path)
 end
 
-#reviews
-    # revtext = {
-    #     1 => "disgusting",
-    #     2 => "not great",
-    #     3 => "meh",
-    #     4 => "pretty good",
-    #     5 => "delicious",
-    # }
 review_list = [[1,'It was disgusting',1,1],
     [3,'meh',2,2],
     [4,'really tasty!',2,1],
