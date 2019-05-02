35fc2b687d7ee4747d660df33d75554d4223b83e
rrios <ryanrios98@gmail.com>
2019-05-02 13:37:29 -0400

    I'm sorry, but the clients wanted more flames.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require turbolinks
 //= require_tree .
 
+
 jQuery(function() {
 
   if (hidden == null) {
@@ -37,5 +38,20 @@ jQuery(function() {
       $("#filter_wrap").hide();
       hidden = true;
     }
+  });
+});
+
+jQuery(function() {
+  $("#funmode").click(function() {
+
+    $("body").css("background-image", "url(/assets/guy-fieiri-flame.jpg)");
+    $("audio")[0].play();
+    alert("Hey everybody I'm Guy Fieri and we're rolling out looking for America's greatest Diners, Drive Ins and Dives!");
+    $(".table").hide()
+    $('h1').removeClass('header').addClass('diablo');
+    $('h2').removeClass('header').addClass('diablo');
+    $('h3').addClass('diablo').text("We're takin' you on a road rockin' trip down to Flavortown, where the gravitational force of bacon warps the laws of space and time.");
+    console.log("congratulations");
+
   });
 });
