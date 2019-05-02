0d926d3b0ec57bfc00c9935dbd287defc5b91760
rrios <ryanrios98@gmail.com>
2019-05-02 19:50:50 -0400

    Revert "Set links on fire with the konami code"
    This reverts commit ed6ed57f33bcd252838c7c96b17d7ce03838b607.
    
    Alex working on more important JavaScript within application.js file, will move link fire code to separate js file.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,18 +55,3 @@ jQuery(function() {
 
   });
 });
-
-$(function(){
-    var kKeys = [];
-    function Kpress(e){
-        kKeys.push(e.keyCode);
-        if (kKeys.toString().indexOf("38,38,40,40,37,39,37,39,66,65") >= 0) {
-            $(this).unbind('keydown', Kpress);
-            firelinks();
-        }
-    }
-    $(document).keydown(Kpress);
-});
-function firelinks(){
-   $('a').toggleClass('diablo');
-};
