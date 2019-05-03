c34cb486c0685698edcb0b2faa5fc30f697f0fa9
Ali Jordahl <ajordahl123@gmail.com>
2019-05-03 14:52:37 -0400

    trying to fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,26 +27,26 @@ private
         params.require(:user).permit(:username, :first, :last, :email, :image)
     end
 
-    def update_star_chef_status
-      if user_signed_in?
-        @user = User.find(params[:id])
+    # def update_star_chef_status
+    #   if user_signed_in?
+    #     @user = User.find(params[:id])
 
-        @count = 0
-            @rating = 0
-            @chefstatus = 0
-            allrecipes = Recipe.where("user_id == ?", @user.id)
-            allrecipes.each do |r|
-                @count = @count + 1
-                if !r.rating.nil?
-                    @rating = @rating + r.rating
-                end
-            end
-        # if average stars for all recipes created by the user is greater than or equal to 4, they are a star chef
-        if @count != 0 && @rating/@count >= 4
-            @chefstatus = 1
-        else
-            @chefstatus = 0
-        end
-    end
+    #     @count = 0
+    #         @rating = 0
+    #         @chefstatus = 0
+    #         allrecipes = Recipe.where("user_id == ?", @user.id)
+    #         allrecipes.each do |r|
+    #             @count = @count + 1
+    #             if !r.rating.nil?
+    #                 @rating = @rating + r.rating
+    #             end
+    #         end
+    #     # if average stars for all recipes created by the user is greater than or equal to 4, they are a star chef
+    #     if @count != 0 && @rating/@count >= 4
+    #         @chefstatus = 1
+    #     else
+    #         @chefstatus = 0
+    #     end
+    # end
   end
 end
